ff7c412e091b3a3a202ea705a115811931f609ff
Prakhar Tripathi <prakhar.tripathi97@gmail.com>
2021-03-05 21:15:10 -0500

    'test'

---
+++ VR/Assets/movement.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class movement : MonoBehaviour
+{
+
+    public float speed = 2f;
+
+    public Rigidbody rb;
+    float moveFB;
+    float moveLR;
+
+
+    void Start()
+    {
+
+        rb = GetComponent<Rigidbody>();
+
+    }
+
+
+    void Update()
+    {
+
+        moveFB = Input.GetAxis("Vertical") * speed;
+        moveLR = Input.GetAxis("Horizontal") * speed;
+
+        Vector3 movement = new Vector3(moveLR, 0, moveFB) * speed * Time.deltaTime;
+
+        rb.MovePosition(transform.position + movement);
+    }
+}
---
+++ VR/Assets/spin.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spin : MonoBehaviour {
+
+    public float spinVal = 90;
+
+	
+	void Start () {
+		
+	}
+	
+	
+	void Update () {
+
+        transform.Rotate(Vector3.up * spinVal * Time.deltaTime);
+
+	}
+
+    public void flipspin() {
+
+        spinVal = -spinVal;
+
+    }
+}
